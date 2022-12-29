e8389f9edf502627c626ab71862bd81c41e133c7
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2022-12-29 12:36:14 +0500

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const routers = require('./routes')
-const port = process.env.port || 5000;
+const port = process.env.port || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const controller = require('../controller/userController');
+const controller = require("../controller/userController");
 
-
-router.get('/', controller.main);
-router.get('/create', controller.userForm);
-router.post('/create', controller.fillForm);
-router.get('/edit/:id', controller.editUser);
-router.post('/update/:id', controller.updateUser);
-router.get('/user/:id', controller.viewUser);
-router.get('/delete/:id', controller.deleteUser);
-
+router.get("/", controller.main);
+router.get("/create", controller.userForm);
+router.post("/create", controller.fillForm);
+router.get("/edit/:id", controller.editUser);
+router.post("/update/:id", controller.updateUser);
+router.get("/user/:id", controller.viewUser);
+router.get("/delete/:id", controller.deleteUser);
 
 module.exports = router;
